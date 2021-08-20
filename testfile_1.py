a635de99a9582a7ea41bfdefca8867bac38f1807
bonnieroderique <bonnie.roderique@urbangridco.com>
2021-08-20 10:10:09 -0400

    changes made without branching

--- testfile_1.py
+++ testfile_1.py
@@ -1,1 +1,3 @@
-""" I made this comment after the initial commit but did not create a branch or redownload the code"""
+""" I made this comment after the initial commit but did not create a branch or redownload the code ---
+
+The result of then committing and pushing was an automatic update to the master code branch"""
